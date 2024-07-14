883c944e0b85dd3e59b09164d1b11a5d91edfcde
micsab <micsab@gmail.com>
2024-07-13 21:27:04 -0700

    day 3 idk what i'm doing

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -15,6 +15,47 @@ class HTMLNODE():
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
     
+class LeafNode(HTMLNODE):
+    def __init__(self, tag=None, value=None, props=None):
+        if value is None:
+            raise ValueError("LeafNode must have a value")
+        super().__init__(tag=tag, value=value, children=None, props=props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("LeafNodes must have a value")
+        
+        props_str = self.props_to_html()
+        if props_str:
+            props_str = " " + props_str
+
+        if self.tag is None:
+            return self.value
+        else:
+            return f"<{self.tag}{props_str}>{self.value}</{self.tag}>"
+        
+class ParentNode(HTMLNODE):
+    def __init__(self, tag=None, children=None, props=None):
+        if tag is None:
+            raise ValueError("ParentNode must have a tag")
+        if children is None or not children:
+            raise ValueError("ParentNode must have children")
+        super().__init__(tag=tag, value=None, children=children, props=props)
+    
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("ParentNode must have a tag")
+        if not self.children:
+            raise ValueError("ParentNode must have children")
+
+        props_str = self.props_to_html()
+        if props_str:
+            props_str = " " + props_str
+
+        children_html = "".join(child.to_html() for child in self.children)
+        return f"<{self.tag}{props_str}>{children_html}</{self.tag}>"
+
+    
 
 def main():
     proppy = {
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNODE
+from htmlnode import HTMLNODE, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_init_default_values(self):
@@ -26,6 +26,29 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNODE("div", "hello", None, {"class": "greeting"})
         expected = "HTMLNode(div, hello, None, {'class': 'greeting'})"
         self.assertEqual(repr(node), expected)
+
+class TestLeafNode(unittest.TestCase):
+    def test_init_defaul_values(self):
+        leaf = LeafNode(value="hello")
+        self.assertIsNone(leaf.tag)
+        self.assertIsNone(leaf.props)
+        self.assertIsNone(leaf.children)
+        self.assertEqual(leaf.value, "hello")
+
+    def test_missing_param(self):
+        with self.assertRaises(ValueError):
+            LeafNode()
+
+    def test_with_tag(self):
+        leaf = LeafNode(tag="span", value="hello", props={"class": "test"})
+        expected_html = '<span class="test">hello</span>'
+        self.assertEqual(leaf.to_html(), expected_html)
+
+    def test_no_tag(self):
+        leaf = LeafNode(value="hello")
+        expected_html = 'hello'
+        self.assertEqual(leaf.to_html(), expected_html)
+
 
 if __name__ == "__main__":
     unittest.main()
