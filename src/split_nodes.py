87ffe063bfda5515241f1ed3c72a82d71379a94a
micsab <micsab@gmail.com>
2024-07-16 22:23:30 -0700

    test

--- src/split_nodes.py
+++ src/split_nodes.py
@@ -31,6 +31,34 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         new_nodes.extend(split_nodes)
     return new_nodes
 
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != text_type_text:
+            new_nodes.append(old_node)
+            continue
+
+        text = old_node.text
+        links = extract_markdown_links(text)
+
+        if not links:
+            new_nodes.append(old_node)
+            continue
+
+        for anchor, url in links:
+            parts = text.split(f"[{anchor}]({url})", 1)
+            if parts[0]:
+                new_nodes.append(TextNode(parts[0], text_type_text))
+            new_nodes.append(TextNode(anchor, text_type_link, url))
+            text = parts[1]
+
+        if text:
+            new_nodes.append(TextNode(text, text_type_text))
+            
+    return new_nodes
+        #print(new_nodes)
+
+
 def extract_markdown_links(text):
     link_pattern = re.compile(r"\[(.*?)\]\((.*?)\)")
     return link_pattern.findall(text) 
@@ -42,8 +70,11 @@ def extract_markdown_image(text):
 
 
 def main():
-    node = TextNode("This is text with a `code block` word", text_type_text)
-    new_nodes = split_nodes_delimiter([node], "`", text_type_code)
+    node = TextNode(
+    "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+    text_type_text,
+    )
+    new_nodes = split_nodes_link([node])
     print(new_nodes)
     
 
